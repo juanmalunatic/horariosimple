fa9a24ef826616c5ea1289916c31a2b80ada97fc
Juan Manuel Luna López <lunalopezjm@gmail.com>
2022-09-22 02:06:52 -0300

    Revamp for 3rd trimester

--- js.js
+++ js.js
@@ -1,16 +1,27 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   
-  hourHeight = 25;
-  tablar(hourHeight);
+  // # Ajustar la altura de la tabla con base en la columna "Duración"
 
-  bar(hourHeight);  
+  // La altura por cada hora de duración. Un evento de duración 1.5 h tiene 1.5 x este valor
+  pixelHeightOneHour  = 25;
+  // El índice de la columna que almacena la duración del bloque. Inicia en 0.
+  columnIndexDuration = 1;
+  setRowHeightAccToDuration(pixelHeightOneHour, columnIndexDuration);
+
+  // # Iniciar la barra naranja que muestra la posición actual del reloj
+
+  drawCurrentTimeBar(pixelHeightOneHour);
+
   setInterval(() => {
-      bar(hourHeight)
+      drawCurrentTimeBar(pixelHeightOneHour)
   }, 1000*60);
+
+  // # Highlight la columna del día actual
+  highlightCurrentDay();
 
   /*
   let cambiarBtn = document.getElementById("submit");
-  let ajusteInput = document.getElementById("ajuste");
+  let ajusteInput = document.getElementByIºd("ajuste");
   cambiarBtn.addEventListener('click', (event) => {
     recalcHoras(ajusteInput.value);
   });
@@ -22,7 +33,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   
 });
 
-function tablar(hourHeight) {
+function setRowHeightAccToDuration(pixelHeightOneHour, columnIndexDuration) {
   
   var table = document.getElementById("tabla");
   for (var i = 0, row; row = table.rows[i]; i++) {
@@ -31,16 +42,19 @@ function tablar(hourHeight) {
     if (i == 0)
       continue;
       
-    // Itero y discrimino tipo de fila
-    let rowtype = row.getAttribute("class") || "bloque";
-    
-    // las "fijo" tienen colspan
-    let indiceDura = (rowtype =="fijo") ? 3 : 4; 
-    
+    // Puedo iterar y discriminar el tipo de fila
+    // - Las evento-colspan son un solo larguero
+    // - Las evento-normal  son bloquesitos sueltos
+    // let rowtype = row.getAttribute("class");
+    // Esto ya no se usa pero para qué lo borro si puede que luego.
+
+    let idxDuration = columnIndexDuration; 
+
+    // Se iteran las columnas para obtener las duraciones
     for (var j = 0, col; col = row.cells[j]; j++) {
-      if (j == indiceDura) {
+      if (j == idxDuration) {
         let factor = parseFloat(col.innerText);
-        let height = hourHeight * factor + "px";
+        let height = pixelHeightOneHour * factor + "px";
         row.style.height = height;
       }
     }  
@@ -48,7 +62,7 @@ function tablar(hourHeight) {
   }
 }
 
-function bar (hourHeight) {
+function drawCurrentTimeBar (pixelHeightOneHour) {
   
   // De 09:00 a 00:30 hay 15.5h
   let horasTot = 15.5;
@@ -76,9 +90,9 @@ function bar (hourHeight) {
   let horasDif = (horaAct - horaMin)/60;
   //console.log("horasDif " + horasDif);
   
-  //console.log(hourHeight * horasDif);
+  //console.log(pixelHeightOneHour * horasDif);
   let barra = document.getElementById("timebar");
-  let heightTot = (hourHeight * horasDif) + "px";
+  let heightTot = (pixelHeightOneHour * horasDif) + "px";
   barra.style.top = heightTot;
 
   // Esto es el pct que puede ser útil para otras vainas
@@ -144,3 +158,83 @@ function recalcHoras(hourMinStrStart) {
   }
   
 }
+
+function highlightCurrentDay()
+{
+
+  const dia = getCurrentWeekDay();
+  // #Método uno
+  //highlightCurrentDayOnCells(dia);
+
+  // #Método dos
+  highlightCurrentDayWithRect(dia);
+}
+
+function getCurrentWeekDay()
+{
+  // Días de 0 a 6 (0: domi, 1:lunes)
+  const d = new Date();
+  let day = d.getDay()
+
+  // Días de 1 a 7, ahora 1:lunes, 7:domingo
+  let dia = (day - 1) == -1 ? 7 : day;
+
+  // Si estamos entre las 00:00 y las 03:00 cuenta como el día anterior
+  let hora = d.getHours();
+  if (hora >= 0 && hora <= 3) {
+    dia--
+  }
+
+  return dia;
+}
+
+// #Método uno: outline en celdas
+function highlightCurrentDayOnCells(currentDay)
+{
+
+  // Iterar la tabla agregando el estilo a todas las filas
+  const colMatch = currentDay + 2; //offset manual :p
+
+  const table = document.getElementById("tabla");
+  for (let r = 0, row; row = table.rows[r]; r++) {
+    for (let c = 0, col; col = row.cells[c]; c++) {
+        if (c == colMatch)
+          col.classList.add("col-current-day");
+    }  
+  }
+}
+
+function highlightCurrentDayWithRect(currentDay)
+{
+  // Get the table
+  const table = document.getElementById("tabla");
+  tableRect = table.getBoundingClientRect();
+
+  // Add a manual offset :)
+  const colMatch = currentDay + 2;
+
+  // Get the column's header and its dimensions
+  currDayColumn = table.rows[0].cells[colMatch];
+  var headerRect = currDayColumn.getBoundingClientRect();
+
+  // Create a rectangle with proper positioning (covering the corresponding column)
+  let rectangle = document.createElement("div");
+  rectangle.setAttribute("id", "overlay-current-day");
+  Object.assign(
+    rectangle.style,
+    {
+      top   : headerRect.top   + window.scrollY + "px",
+      left  : headerRect.left  + "px",
+      width : headerRect.width + "px",
+      height: tableRect.height + "px"
+    }
+  );
+
+  document.querySelector("body").appendChild(rectangle);
+
+  // Debug
+  //Object.assign(rectangle.style, {backgroundColor:"#f90000"})
+  //rectangle.innerHTML = "Hola";
+
+}
+
